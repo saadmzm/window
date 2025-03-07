6d311d0a137fc87b0af2564afe73e9727fe8c7f3
Saad Moazzam <saad.mzm65@gmail.com>
2025-03-07 17:27:54 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+    }
+    fn window_event(
+            &mut self,
+            event_loop: &ActiveEventLoop,
+            id: WindowId,
+            event: WindowEvent,
+        ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    
+    event_loop.run_app(&mut app);
+}
